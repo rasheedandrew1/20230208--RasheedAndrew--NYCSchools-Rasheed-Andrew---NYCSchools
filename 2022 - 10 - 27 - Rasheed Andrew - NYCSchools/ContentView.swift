cadab189da238d307ea8aa408cd7a728d553780c
rasheed <rasheedandrew1@gmail.com>
2022-10-27 15:18:27 -0400

    Initial Commit

---
+++ 2022 - 10 - 27 - Rasheed Andrew - NYCSchools/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  2022 - 10 - 27 - Rasheed Andrew - NYCSchools
+//
+//  Created by rasheed andrew on 10/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 2022 - 10 - 27 - Rasheed Andrew - NYCSchools/_022___10___27___Rasheed_Andrew___NYCSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _022___10___27___Rasheed_Andrew___NYCSchoolsApp.swift
+//  2022 - 10 - 27 - Rasheed Andrew - NYCSchools
+//
+//  Created by rasheed andrew on 10/27/22.
+//
+
+import SwiftUI
+
+@main
+struct _022___10___27___Rasheed_Andrew___NYCSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
